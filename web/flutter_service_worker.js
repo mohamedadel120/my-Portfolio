061f3a00b4d1a0695ac5c6fd879cea3a3990dd5c
mohamed Adel <50755506+mohamedadel120@users.noreply.github.com>
2026-01-14 14:52:32 +0200

    this is enhancment

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -72,7 +72,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ac69e0e48c418d7d445598717377568f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f7db42fb90eed4ecda9fb9ef8c17ff23",
+"flutter_bootstrap.js": "7b894a906ce1251eb6c324ab4f30d43d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
