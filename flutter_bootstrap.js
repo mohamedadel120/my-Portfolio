9485d031094d695f3db05326e2f35c3f8be53dba
mohamedadel120 <50755506+mohamedadel120@users.noreply.github.com>
2026-05-04 13:20:36 +0000

    Deploying to gh-pages from @ mohamedadel120/my-Portfolio@8103a7ad8164bb06f30a8ac0ce0caebb80b27c0c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -10,7 +10,17 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "3319162589"
+  onEntrypointLoaded: async function(engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    
+    // Hide the loader container smoothly
+    const loader = document.querySelector('.loader-container');
+    if (loader) {
+      loader.style.transition = 'opacity 0.5s ease-out';
+      loader.style.opacity = '0';
+      setTimeout(() => loader.remove(), 500);
+    }
+    
+    await appRunner.runApp();
   }
 });
